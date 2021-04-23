43624be25c422fc37c293d21b2f3801ac25c8fcb
Fred Skaggs <fskaggs@outlook.com>
2021-04-22 20:00:26 -0700

    Added a password strength progress bar for fun.

--- Homework1/Homework1/MainWindow.xaml.cs
+++ Homework1/Homework1/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace Homework1
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int passwordStrength { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,11 +30,25 @@ namespace Homework1
 
         private void uxSubmit_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show($"Hello, {uxName.Text}\nYou entered password: {uxPassword.Text}");
+            string strength = (passwordStrength < 40) ? "not very strong" : "strong";
+            string message = $"Hello, {uxName.Text}\nYou entered password: {uxPassword.Text}\nYour password is {strength}";
+            MessageBox.Show(message);
         }
 
         private void ux_InputRecieved(object sender, TextChangedEventArgs e)
         {
+            TextBox tb = sender as TextBox;
+
+            if (tb.Name == "uxPassword")
+            {
+                string password = uxPassword.Text;
+                int upperScore = password.Where(x => char.IsUpper(x)).Count<char>() > 0 ? 10 : 0;
+                int lowerScore = password.Where(x => char.IsLower(x)).Count<char>() > 0 ? 10 : 0;
+                int digitScore = password.Where(x => char.IsDigit(x)).Count<char>() > 0 ? 10 : 0;
+                passwordStrength = Math.Clamp(password.Length, 0, 20) + upperScore + lowerScore + digitScore;
+                progBarPwdStr.Value = passwordStrength;
+            }
+
             if (!string.IsNullOrEmpty(uxName.Text) && !string.IsNullOrEmpty(uxPassword.Text))
             {
                 uxSubmit.IsEnabled = true;
