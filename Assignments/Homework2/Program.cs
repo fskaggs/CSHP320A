dfee2912a7138b6a938bddaa67671b6af8d7fbba
Fred Skaggs <fskaggs@outlook.com>
2021-05-03 20:51:19 -0700

    Finalized homework 2.

--- Assignments/Homework2/Program.cs
+++ Assignments/Homework2/Program.cs
@@ -17,20 +17,21 @@ namespace Homework2
             users.Add(new Models.User { Name = "Steve", Password = "steve" });
             users.Add(new Models.User { Name = "Lisa", Password = "hello" });
 
+            // Display the users who's password is 'Hello'
             Console.WriteLine("Users whose password is 'Hello':");
             users.Where(x => x.Password == "hello").ToList().ForEach(x => Console.WriteLine(x.Name));
 
-            // Remove users where password is their name in lower case
-            Console.WriteLine("\nRemoving users where the password is their name in lower case.");
+            // Delete any passwords that are the lower-cased version of their Name.
+            Console.WriteLine("\nDeleting passwords which are the user's name in lower case.");
             users.FindAll(x => x.Password == x.Name.ToLower()).ForEach(x => x.Password = "");
 
-            // Delete first user where password is 'hello'
-            Console.WriteLine("Deleting the first user who's password is 'hello'.");
+            // Delete first user that has the password 'hello'
+            Console.WriteLine("Deleting the first user that has the password 'hello'.");
             users.Remove(users.FirstOrDefault(x => x.Password == "hello"));
 
             // Display the remaining users
             Console.WriteLine("\nThese are the remaining users.");
-            users.Where(x => x.Password == "hello").ToList().ForEach(x => Console.WriteLine($"Name: {x.Name}  Password: {x.Password}"));
+            users.ForEach(x => Console.WriteLine($"Name: {x.Name}  Password: {x.Password}"));
         }
     }
 }
