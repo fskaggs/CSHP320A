c41a7b69f54e798c5402ad88c4c03200f03a3ec2
Fred Skaggs <fskaggs@outlook.com>
2021-05-13 12:40:35 -0700

    Homework 4 final version using event instead of bindings

--- Assignments/Homework4/MainWindow.xaml.cs
+++ Assignments/Homework4/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Homework4.Models;
+﻿using System.Text.RegularExpressions;
 using System.Windows;
 
 namespace Homework4
@@ -8,12 +8,24 @@ namespace Homework4
     /// </summary>
     public partial class MainWindow : Window
     {
-        private ZipCode zipCode = new ZipCode();
-
         public MainWindow()
         {
             InitializeComponent();
-            stackPanelZipCode.DataContext = zipCode;
+        }
+
+        private void tbZipCode_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            bool code = false;
+            Regex regexZipCode = new Regex(@"^[0-9]{5}(?:-[0-9]{4})?$");
+            Regex regexPostCode = new Regex(@"^(?!.*[DFIOQU])[A-VXY][0-9][A-Z]●?[0-9][A-Z][0-9]$");
+            string postCode = tbZipCode.Text.ToUpper();
+            
+            if (string.IsNullOrEmpty(postCode) == false)
+            {
+                code = regexZipCode.IsMatch(postCode) | regexPostCode.IsMatch(postCode);
+            }
+          
+            btnZipCode.IsEnabled = code;
         }
     }
 }
